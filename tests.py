c6b0aaa00ae6aedc9c1f112d105fc2505e518bb9
Ethan Lewis <eclewis21@gmail.com>
2014-07-06 16:56:27 -0700

    Added inverse stubs and provided flag to determine encrypt/decrypt

--- tests.py
+++ tests.py
@@ -16,9 +16,10 @@ def run_tests(exe):
     print()
 
     for test in sorted(list(tests)):
-        subprocess.Popen(
+        proc = subprocess.Popen(
             [
                 "./{}".format(exe),
+                "-e" if test[7:10] == "enc" else "-d",
                 "-b", test[3:6],
                 "-k", test_path(test, "key"),
                 "-i", test_path(test, "in"),
@@ -26,28 +27,29 @@ def run_tests(exe):
             ],
             stdout = subprocess.PIPE,
             stderr = subprocess.STDOUT
-        ).communicate()[0]
-
+        )
+        out = proc.communicate()[0]
         print_diff(test, test_path(test, "out"), test_path(test, "ref"))
 
 def test_path(name, typ):
     return "{}/{}.{}".format(TESTDIR, name, typ)
 
 def print_diff(test, fout, fref):
+    output = refput = ""
+    if os.path.exists(fout):
+        with open(fout, "rb") as fout_f:
+            output = fout_f.read().decode("UTF-8")
+    if os.path.exists(fref):
+        with open(fref, "rb") as fref_f:
+            refput = fref_f.read().decode("UTF-8")
+
     print(test.ljust(20) + ":", end=" ")
-    with open(fout, "rb") as fout_f:
-        output = fout_f.read().decode("UTF-8")
-        if os.path.exists(fref):
-            with open(fref, "rb") as fref_f:
-                refput = fref_f.read().decode("UTF-8")
-                if (output == refput):
-                    print("PASS")
-                else:
-                    print("FAIL")
-                    print("   ref: {}".format(refput))
-                    print("   usr: {}".format(output))
-        else:
-            print("REF not found. Output was {}".format(output))
+    if (output == refput):
+        print("PASS")
+    else:
+        print("FAIL")
+        print("   ref: {}".format(refput))
+        print("   usr: {}".format(output))
         
 
 def main():
